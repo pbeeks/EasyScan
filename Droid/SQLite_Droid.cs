b5583d3b65f5d1eb80475e399fd9f00a36f97090
Patrick Buechler <buechler.pat@gmail.com>
2016-07-29 15:17:28 -0500

    Android working.

---
+++ Droid/SQLite_Droid.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using BasicScanner.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_Droid))]
+
+namespace BasicScanner.Droid
+{
+	public class SQLite_Droid : ISQLite
+	{
+		public SQLite_Droid() { }
+		public SQLite.SQLiteConnection GetConnection()
+		{
+			var sqliteFilename = "BasicScannerSQLite.db3";
+			var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // Documents folder
+			var path = Path.Combine(documentsPath, sqliteFilename);
+			// Create the connection
+			var conn = new SQLite.SQLiteConnection(path);
+			// Return the database connection
+			return conn;
+		}
+	}
+}
